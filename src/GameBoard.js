c1662e8b2568744a3c72b9ab9c471e0639b9b09d
Luke <baslilonfive@gmail.com>
2018-07-04 14:35:50 -0500

    add display discs

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class GameBoard extends Component {
+    
+  
   render() {
+    const { discs,onPlaceDisc } = this.props 
+    
     return (
       <div className="game-board">
-
-
+        <button onClick={() => onPlaceDisc(0)}>🡻</button>
+        <button onClick={() => onPlaceDisc(1)}>🡻</button>
+        <button onClick={() => onPlaceDisc(2)}>🡻</button>
+        <button onClick={() => onPlaceDisc(3)}>🡻</button>
+        <button onClick={() => onPlaceDisc(4)}>🡻</button>
+        <button onClick={() => onPlaceDisc(5)}>🡻</button>
+        <button onClick={() => onPlaceDisc(6)}>🡻</button>
+        <table className="table">
+          <tbody>
+            {discs.map((unit,index)=>(
+              <tr key={index} className="tr">
+               {unit.map((disc, index)=>(
+                 <td style={{backgroundColor: disc ? disc === 1 ? 'red': 'blue' : 'white'}} key={index}>O</td>
+               ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
